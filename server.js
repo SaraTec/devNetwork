5bdde315716371b502a15baff3eb063644caae2b
SaraTec <cosmo.epik@gmail.com>
2021-01-08 13:22:09 +0200

    User registration

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
 
+// Connect Databese
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }))
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running'))
 
-app.listen(PORT, ()=>{
+//Define Routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 });
