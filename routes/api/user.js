2996dbb1a49b040c150570c5bc99fb78cbdb143a
SaraTec <cosmo.epik@gmail.com>
2021-01-13 15:42:57 +0200

    implement JWT in registration

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,9 @@ const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar');
 const User = require('../../Models/User');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
@@ -12,18 +15,18 @@ router.post('/', [
     .not()
     .isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please enter a password with 6 or more characters').isLength({min:6}),
-],async (req, res) => {
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+], async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
   try {
-    let user = await User.findOne({email});
+    let user = await User.findOne({ email });
 
-    if(user) {
+    if (user) {
       return res.status(400).json({
         errors: [
           {
@@ -51,7 +54,24 @@ router.post('/', [
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
-    res.send('User registered')
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token)=>{
+          if(err) throw err;
+          res.json({token})
+        }
+      )
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server error')
