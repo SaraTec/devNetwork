e6d41773d8b4f252eebc9200bda57a0721550300
SaraTec <cosmo.epik@gmail.com>
2021-03-16 19:53:46 +0200

    Starting on Dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,8 +7,17 @@ import { logout } from '../../actions/auth'
 export const Navbar = ({ isAuthenticated, loading, logout }) => {
   const authLinks = (
     <ul>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{' '}
+          <span className='hide-sm'>
+              Dashboard
+          </span>
+        </Link>
+      </li>
       <li><Link onClick={logout} to="#">
         <i className="fas fa-sign-out-alt"></i>
+        {' '}
         <span className='hide-sm'>Logout</span>
       </Link></li>
     </ul>
