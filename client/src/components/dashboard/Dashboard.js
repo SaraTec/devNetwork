0a6e08d0024cc83ba5db4f16c55c065381556e66
SaraTec <cosmo.epik@gmail.com>
2021-03-30 19:17:58 +0300

    Create & Edit Profiles

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom'
+import  DashboardAction  from './DashboardAction'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
@@ -18,14 +19,16 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         <i className="fas fa-user"></i>
         {"  "}Welcomes {user && user.name}
       </p>
-      {profile !== null 
-      ? <Fragment>has</Fragment> 
-      : <Fragment>
-        <p>You have not yet setup a profile, please add some info</p>
-        <Link to ='/create-profile' className='btn btn-primary my-1'>
-          Create Profile
+      {profile !== null
+        ? <Fragment>
+          <DashboardAction />
+        </Fragment>
+        : <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
         </Link>
-      </Fragment> 
+        </Fragment>
       }
     </Fragment>)
 }
