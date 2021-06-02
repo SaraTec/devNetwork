324182cef5a3e1b650e3209044f49a530028ed26
SaraTec <cosmo.epik@gmail.com>
2021-06-02 23:07:22 +0300

    Display Profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,11 @@ export const Navbar = ({ isAuthenticated, loading, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user"></i>{' '}
           <span className='hide-sm'>
@@ -25,7 +30,7 @@ export const Navbar = ({ isAuthenticated, loading, logout }) => {
 
   const guesLinks = (
     <ul>
-      <li><Link to="#">Developers</Link></li>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -43,7 +43,6 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
-        loading: false
       }
     case GET_REPOS:
       return {
