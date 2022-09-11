2e4eae36cbae2aec523fe7048da0d79d63ce15f2
SaraTec <cosmo.epik@gmail.com>
2022-09-11 14:08:56 +0300

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const path = require('path')
 
 const app = express();
 const http = require("http");
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }))
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API Running'))
+//app.get('/', (req, res) => res.send('API Running'))
 
 //Define Routes
 app.use('/api/users', require('./routes/api/user'));
@@ -69,6 +69,16 @@ io.on('connection', socket => {
     }
   });
 });
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
